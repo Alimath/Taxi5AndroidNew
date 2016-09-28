ef91cc4bf4c60bd0c94ab9836d6127459e9ad653
Fedar Trukhan <fedar.trukhan@Alexanders-iMac.local>
2016-09-28 19:28:34 +0300

    Assist 60%

--- app/src/main/java/com/isolutions/taxi5/FragmentPayments.java
+++ app/src/main/java/com/isolutions/taxi5/FragmentPayments.java
@@ -1,6 +1,7 @@
 package com.isolutions.taxi5;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,12 +9,26 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Xml;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.isolutions.taxi5.API.Taxi5SDKEntity.ProfileData;
+
+import java.util.Map;
+import org.apache.http.util.EncodingUtils;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -56,28 +71,68 @@ public class FragmentPayments extends Fragment {
             builder = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Material_Dialog_Alert);
         }
 
-//        TextView msgTextView = new TextView(AppData.getInstance().getAppContext());
-//        msgTextView.setTextSize(20);
-//        msgTextView.setPadding(0,16,0,16);
-//        msgTextView.setTextColor(AppData.getInstance().getColor(R.color.defaultBlack));
-//        msgTextView.setText();
-
-
-//        msgTextView.setGravity(Gravity.CENTER);
-
-//        builder.setView(msgTextView);
-        builder.setTitle("Соглашение");
+        builder.setTitle(getString(R.string.payments_agreement_title));
         builder.setMessage(getString(R.string.payments_agreement));
-//        builder.
-        builder.setPositiveButton(R.string.status_car_on_way_call_driver_dialog_ok, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(R.string.payments_agreement_ok_button, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-//                Intent intent = new Intent(Intent.ACTION_CALL);
-//                intent.setData(Uri.parse("tel:+375291337500"));
-//                startActivity(intent);
+                AlertDialog.Builder alert = new AlertDialog.Builder(AppData.getInstance().mainActivity);
+                alert.setTitle(getString(R.string.payments_web_view_add_card_title));
+
+                LinearLayout wrapper = new LinearLayout(AppData.getInstance().mainActivity);
+                EditText keyboardHack = new EditText(AppData.getInstance().mainActivity);
+
+                keyboardHack.setVisibility(View.GONE);
+
+
+                WebView wv = new WebView(AppData.getInstance().mainActivity);
+
+                String currentDateString = ((Long)(System.currentTimeMillis()/1000)).toString();
+                ProfileData profileData = ProfileData.getInstance();
+
+                String postData = "Merchant_ID=460330&OrderNumber=taxi5_test_auth_"+currentDateString+"&OrderAmount=1&OrderComment=КОММЕНТАРИЙ&OrderCurrency=BYN&Lastname=Трухан Федор";
+                if(!TextUtils.isEmpty(profileData.getMsid())) {
+                    postData += "&CustomerNumber="+profileData.getMsid();
+                }
+                if(!TextUtils.isEmpty(profileData.getName())) {
+                    postData += "&Firstname="+profileData.getName();
+                }
+//                if(!TextUtils.isEmpty(profileData.getEmail())) {
+//                    postData += "&Email="+profileData.getEmail();
+//                }
+
+                wv.postUrl("https://pay140.paysec.by/pay/order.cfm", EncodingUtils.getBytes(postData, "BASE64"));
+                wv.setWebViewClient(new WebViewClient() {
+//                    @Override
+//                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                        view.loadUrl(url);
+//
+//                        return true;
+//                    }
+
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                        return super.shouldOverrideUrlLoading(view, request);
+                    }
+                });
+
+                alert.setView(wv);
+                alert.setNegativeButton(getString(R.string.status_car_on_way_call_driver_dialog_cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+                wrapper.setOrientation(LinearLayout.VERTICAL);
+                wrapper.addView(wv, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                wrapper.addView(keyboardHack, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                alert.setView(wrapper);
+                alert.create().show();
             }
         });
-        builder.setNegativeButton(R.string.status_car_on_way_call_driver_dialog_cancel, new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(R.string.payments_agreement_cancel_button, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 
